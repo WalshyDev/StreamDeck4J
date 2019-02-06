b9d0fceccd208e92db5334cde2eb9f63f301ff2a
WalshyDev <walshydev@gmail.com>
2019-02-06 21:44:27 +0000

    Fix build error

--- src/main/java/com/walshydev/streamdeck4j/info/Application.java
+++ src/main/java/com/walshydev/streamdeck4j/info/Application.java
@@ -4,14 +4,11 @@ import lombok.Getter;
 
 import javax.annotation.Nonnull;
 
-@Getter(onMethod = @__({@Nonnull}))
+@Getter(onMethod_ = {@Nonnull})
 public class Application {
 
-    @Nonnull
     private String language;
-    @Nonnull
     private String platform;
-    @Nonnull
     private String version;
 
     public Application(@Nonnull String language, @Nonnull String platform, @Nonnull String version) {
