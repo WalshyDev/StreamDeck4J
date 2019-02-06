d437b28bc84f9fab54449b1a3b5aa4117ee6f7fb
WalshyDev <walshydev@gmail.com>
2019-02-06 18:37:03 +0000

    Re-add the documentation for this.

--- src/main/java/com/walshydev/streamdeck4j/StreamDeck4J.java
+++ src/main/java/com/walshydev/streamdeck4j/StreamDeck4J.java
@@ -362,7 +362,6 @@ public class StreamDeck4J {
     /////////////////////////
     // Public methods
     /////////////////////////
-
     /**
      * Adds an event listener to the plugin
      *
@@ -390,6 +389,11 @@ public class StreamDeck4J {
         return this.devices;
     }
 
+    /**
+     * Opens a URL on the PC the Stream Deck is connected to.
+     *
+     * @param url The URL to open
+     */
     public void openURL(@Nonnull URL url) {
         logger.trace("openURL(url)");
         JsonObject payload = new JsonObject();
